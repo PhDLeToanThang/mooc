3b8ddc4216f972de1e73f44786b1a196506b7b0a
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2022-07-25 22:50:52 +0700

    Update add_multi_moodle4.sh

--- add_multi_moodle4.sh
+++ add_multi_moodle4.sh
@@ -1,18 +1,18 @@
 clear
 cd ~
 ############### Tham số cần thay đổi ở đây ###################
-echo "FQDN: e.g: demo.company.vn"   # Đổi địa chỉ web thứ nhất (Website Master for Resource code - để tạo cùng 1 Source code duy nhất 
+echo "FQDN"   # Đổi địa chỉ web thứ nhất (Website Master for Resource code - để tạo cùng 1 Source code duy nhất 
 read -e FQDN
 
-echo "dbname: e.g: demodata"   # Tên DBNane
+echo "dbname"   # Tên DBNane
 read -e dbname
-echo "dbuser: e.g: userdata"   # Tên User access DB lmsatcuser
+echo "dbuser"   # Tên User access DB lmsatcuser
 read -e dbuser
-echo "Database Password: e.g: P@$$w0rd-1.22"
+echo "dbpass"
 read -s dbpass
-echo "phpmyadmin folder name: e.g: phpmyadmin"   # Đổi tên thư mục phpmyadmin khi add link symbol vào Website 
+echo "phpmyadmin"   # Đổi tên thư mục phpmyadmin khi add link symbol vào Website 
 read -e phpmyadmin
-echo "Moodle Folder Data: e.g: moodledata"   # Tên Thư mục chưa Data vs Cache
+echo "FOLDERDATA"   # Tên Thư mục chưa Data vs Cache
 read -e FOLDERDATA
 
 dbtype="mariadb"
