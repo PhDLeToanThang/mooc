3b2b60cd9b34e30bc62eb9de1add7f0939c7e48a
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2022-10-21 10:50:25 +0700

    Update update_moodle.sh

--- update_moodle.sh
+++ update_moodle.sh
@@ -4,22 +4,22 @@ echo "FQDN: e.g: demo.company.vn"   # Đổi địa chỉ web thứ nhất (Webs
 read -e folder_webmoodle
 
 # switch to moodle source directory
-cd /usr/share/nginx/html/$folder_webmoodle;
+cd /var/www/nginx/html/$folder_webmoodle;
 
 # put moodle into maintenance mode
-sudo -u www-data php /usr/share/nginx/html/$folder_webmoodle/admin/cli/maintenance.php --enable;
+sudo -u www-data php /var/www/html/$folder_webmoodle/admin/cli/maintenance.php --enable;
 
 # pull changes
 git pull;
 
 # run the moodle upgrade script to which you will have to answer: y
-sudo -u www-data php /usr/share/nginx/html/$folder_webmoodle/admin/cli/upgrade.php;
+sudo -u www-data php /var/www/html/$folder_webmoodle/admin/cli/upgrade.php;
 
 # Put moodle into regular mode again
-sudo -u www-data php /usr/share/nginx/html/$folder_webmoodle/admin/cli/maintenance.php --disable;
+sudo -u www-data php /var/www/html/$folder_webmoodle/admin/cli/maintenance.php --disable;
 
 # run the cron script to clean up
-sudo -u www-data php /usr/share/nginx/html/$folder_webmoodle/admin/cli/cron.php;
+sudo -u www-data php /var/www/html/$folder_webmoodle/admin/cli/cron.php;
 
 # and switch back to your original directory
 cd -;
